abd7a6b1d5281321b61a80d264a9acd77852eb30
fedeminatta <minattafederico@gmail.com>
2022-11-02 11:24:59 -0300

    s

--- app.js
+++ app.js
@@ -7,37 +7,46 @@ const sizeInput = document.querySelector('#size');
 
 // Añadir notas tocando el boton añadir
 addBtn.addEventListener('click', (e) => {
-  if (textArea.value === '') {
-    alert('Por favor escribe una nota');
-    box.remove();
-  }
+    if (textArea.value === '') {
+        alert('Por favor escribe una nota');
+        box.remove();
+    }
 
-  const box = document.createElement('div');
-  box.className = 'box';
-  const text = document.createElement('p');
-  const closeBtn = document.createElement('button');
+    const box = document.createElement('div');
+    box.className = 'box';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('toggle');
+    const text = document.createElement('p');
+    const closeBtn = document.createElement('button');
 
-  notes.appendChild(box);
-  box.appendChild(text);
-  box.appendChild(closeBtn);
+    notes.appendChild(box);
+    box.appendChild(checkbox);
+    box.appendChild(text);
+    box.appendChild(closeBtn);
 
-  closeBtn.innerHTML = 'X';
-  closeBtn.addEventListener('click', () => {
-    box.remove();
-  });
-  text.innerHTML = textArea.value;
-  text.style.color = colorInput.value;
-  text.style.fontSize = sizeInput.value + 'px';
-  if (textArea.value === text.innerHTML) {
-    textArea.value = '';
-  }
+    closeBtn.innerHTML = 'X';
+    closeBtn.addEventListener('click', () => {
+        box.remove();
+    });
+    text.innerHTML = textArea.value;
+    text.style.color = colorInput.value;
+    text.style.fontSize = sizeInput.value + 'px';
+    if (textArea.value === text.innerHTML) {
+        textArea.value = '';
+    }
+
+    // funcion para que el checkbox cambie el fondo del box
+    checkbox.addEventListener('change', function () {
+        box.style.background = this.checked ? '#85CB33' : '#fff';
+    });
 });
 
 // Añadir el color al textarea
 colorInput.addEventListener('change', () => {
-  textArea.style.color = colorInput.value;
+    textArea.style.color = colorInput.value;
 });
 // Añadir font size al textarea
 sizeInput.addEventListener('change', () => {
-  textArea.style.fontSize = sizeInput.value + 'px';
+    textArea.style.fontSize = sizeInput.value + 'px';
 });
